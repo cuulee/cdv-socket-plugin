7c61393b9639c6e8afb7345e152bc28b3f27d6fe
Diogo Queirós <diogo.queiros@tlantic.com>
2017-11-13 18:43:14 +0000

    test

--- src/android/SocketPlugin.java
+++ src/android/SocketPlugin.java
@@ -154,9 +154,9 @@ public class SocketPlugin extends CordovaPlugin {
 
 Charset latin1Charset = Charset.forName("Windows-1252"); 
 CharBuffer charBuffer = latin1Charset.decode(ByteBuffer.wrap(decodedData));
-data = charBuffer.toString();
+//data = charBuffer.toString();
 data = String.valueOf(charBuffer);
-data = String.valueOf(decodedData);
+//data = String.valueOf(decodedData);
 
                                         socket.write(data);
 
